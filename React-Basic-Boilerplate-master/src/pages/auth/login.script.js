87507c64904512bb4fde93007e60b4f18fa2eb30
Achini Nisansala <achininisansala069@gmail.com>
2021-07-14 01:15:51 +0530

    Add files via upload

---
+++ React-Basic-Boilerplate-master/src/pages/auth/login.script.js
@@ -0,0 +1,16 @@
+export default function loginBackgroundActions()
+{
+    const signInBtn = document.querySelector("#sign-in-btn");
+    const signUpBtn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".container");
+
+    signUpBtn.addEventListener("click", () =>
+    {
+        container.classList.add("sign-up-mode");
+    });
+
+    signInBtn.addEventListener("click", () =>
+    {
+        container.classList.remove("sign-up-mode");
+    });
+}
